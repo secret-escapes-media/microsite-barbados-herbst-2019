eb1fda4e861a5e1e8aca9c493cfbff771471d958
Birgit <birgit.voslamber@secretescapes.com>
2019-09-04 12:39:53 +0200

    update images and deal

--- _assets/js/_components/modal.js
+++ _assets/js/_components/modal.js
@@ -1,5 +1,4 @@
 
-
 // =========================================
 //  MODAL
 // =========================================
@@ -36,8 +35,7 @@ function modalOpen(event, modalId){
     // update video
     if( $(modalItem).attr('data-video-id') ){
       var modalVideoId = $(modalItem).data('video-id');
-      //$('#modal-video__'+modalItemID).attr('src', 'https://www.youtube.com/embed/' + modalVideoId + '?enablejsapi=1&controls=1&rel=0&showinfo=0&modestbranding=1');
-    //  $('#modal-video__'+modalItemID).attr('src', 'https://www.youtube.com/embed/' + modalVideoId + '?enablejsapi=1&controls=1&rel=0&showinfo=0&modestbranding=1');
+      $('#modal-video__'+modalItemID).attr('src', 'https://www.youtube.com/embed/' + modalVideoId + '?enablejsapi=1&controls=1&rel=0&showinfo=0&modestbranding=1');
 
     }
 
@@ -45,7 +43,6 @@ function modalOpen(event, modalId){
     // QUERY - If modal opened from querystring
     modalItem = '.modal__item--' + modalId;
   }
-
 
   // hides all modal content
   $('.modal__item').addClass('is-closed').hide();
@@ -71,7 +68,17 @@ function modalOpen(event, modalId){
 function modalClose(event){
   event.preventDefault();
   $('body').removeClass('disable-scroll');
-
+  var stopVideo = function ( element ) {
+      var iframe = element.querySelector( 'iframe');
+      var video = element.querySelector( 'video' );
+      if ( iframe !== null ) {
+          var iframeSrc = iframe.src;
+          iframe.src = iframeSrc;
+      }
+      if ( video !== null ) {
+          video.pause();
+      }
+  };
   // Reset scroll position
   setTimeout(function() {
     $('.modal__wrap').scrollTop(0);
@@ -82,6 +89,7 @@ function modalClose(event){
     $(this).removeClass('is-open').removeClass('modal--carousel').addClass('is-closed');
     $('.modal__item.is-open').removeClass('is-open').addClass('is-closed');
   });
+
 
   // disable video
   $('.modal__item.is-open .modal-video__iframe').attr('src','https://www.youtube.com/embed/');
@@ -229,100 +237,3 @@ $(document).on('keyup', function(e) {
     }
   }
 });
-
-
-
-/*
-var modal          = $('.js-modal'),
-    modalLaunchBtn = $('.js-open-modal'),
-    modalCloseBtn  = $('.js-close-modal');
-
-// opens modal
-function modalOpen(event, modalId){
-
-  // is there a click event?
-  if (event) {
-    event.preventDefault();
-    // find the modal id from clicked element
-    var activeModalId = $(event.currentTarget).data('open-modal');
-  } else {
-    // find the modal id from passed string
-    var activeModalId = modalId;
-  }
-
-  // find the active modal dom element
-  var activeModal = $('*[data-modal-id="' + activeModalId + '"]');
-
-  // disable scrolling on background content (doesn't work iOS)
-  $('body').addClass('disable-scroll');
-
-  // builds youtube video if needed
-  if (activeModal.data('youtube-id')) {
-    // get youtube id and target div
-    var video     = activeModal.find('.js-modal-video'),
-        youtubeId = activeModal.data('youtube-id');
-    // insert the code into the target with the id and autoplay
-    video.html('<div class="video__wrap"><iframe class="video" src="https://www.youtube.com/embed/' + youtubeId + '?rel=0&amp;showinfo=0&autoplay=1" frameborder="0" allowfullscreen="allowfullscreen"></iframe></div>');
-  }
-
-  // reveal the specific modal content
-  activeModal.removeClass('is-closed').addClass('is-open');
-
-  // open modal
-  modal.fadeIn('250', function(){
-    $(this).removeClass('is-closed').addClass('is-open');
-  });
-}
-
-// closes modal
-function modalClose(event){
-  event.preventDefault();
-  // enable scrolling
-  $('body').removeClass('disable-scroll');
-  // close modal with fade
-  $('.modal__bg.is-open').fadeOut('250', function(){
-    // close modal and active modal content
-    $(this).removeClass('is-open').addClass('is-closed');
-    $('.modal.is-open').removeClass('is-open').addClass('is-closed');
-    // kill everything inside of video if its there
-    $('.js-modal-video').empty();
-  });
-}
-
-
-// launches modal when offer is clicked
-modalLaunchBtn.on('click', function(event) {
-  modalOpen(event);
-});
-
-// launches modal from url queryString
-var modalQueryString = 'open-modal';
-if (getQueryStringByName(modalQueryString)) {
-  // find modal id & dom element
-  var modalId = getQueryStringByName(modalQueryString);
-  var modalElement = $('*[data-modal-id="' + modalId + '"]');
-  // is there is a modal to open?
-  if ( $(modalElement).length > 0 ) {
-    // open without passing event
-    modalOpen(null, modalId);
-  }
-}
-
-// closes modal on close icon click
-modalCloseBtn.on('click', function(event) {
-  modalClose(event);
-});
-
-// closes modal on background click
-modal.on('click', function(event) {
-  if ($(event.target).hasClass('modal__wrap')){
-    modalClose(event);
-  }
-});
-
-// closes modal on escape key press
-$(document).keyup(function(event) {
-   if (event.keyCode == 27) {
-     modalClose(event);
-    }
-});*/
